9aa250873234a56c4c3d8461a718fcfa1ae52064
Maggie Lee <maggie.a.lee@gmail.com>
2025-07-13 20:31:56 +0000

    Adding first python file

---
+++ python_files/first_python_file.py
@@ -0,0 +1,48 @@
+print('Hello World')
+
+
+# Any line that starts with '#' is a COMMENT.
+# Comment lines are for humans to read, the computer won't read them
+# Leave notes for yourself or the next person who might use this code.
+# Well-commented code is a blessing.
+
+# The next bit is real code. It's some instructions to the computer.
+# "Print" means "write something in the terminal panel down there below"
+# What would you like to print? Fill in some comment between the '' marks
+
+print('')
+
+# By the way, words that not instructions to the computer always need to be in ' '
+#  or  " "  marks. 
+
+# UNcomment the next line, hit Run, and see what happens
+# print(Ida B. Wells)
+
+# OK fix that line above and re-run.
+
+# Let's mess around some more.
+# Uncomment this next section:
+
+# print('What is your name?')
+# user_name = input()
+# print('Ahoy there, ' + user_name)
+# print('The length of your name is:')
+# print(len(user_name))
+
+# OK great, now UNcomment this next section too
+
+# print('Please give me a number between 1 and 10:')
+# some_number = input()
+# half_number = (int(some_number)/2)
+# print('Half of that is:')
+# print(half_number)
+
+# int() ?? What is that?
+# A function!  A built-in piece of code that comes with Python.
+# It's short for INTERGER.  You're telling the computer:
+# "Hey, everything in these parentheses is a number."
+# Before we go on, let's check out some other built-in functions at the
+# official Python page:
+# https://docs.python.org/3/library/functions.html
+
+# What other built-in functions have we already used?
