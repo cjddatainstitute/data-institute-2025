5af96e4581a185033e600cbce38caf54d325a00d
Maggie Lee <maggie.a.lee@gmail.com>
2025-07-13 21:42:25 +0000

    Added more examples

--- python_files/first_python_file.py
+++ python_files/first_python_file.py
@@ -31,7 +31,7 @@ print('')
 
 # OK great, now UNcomment this next section too
 
-# print('Please give me a number between 1 and 10:')
+# print('Please give me a whole number between 1 and 10:')
 # some_number = input()
 # half_number = (int(some_number)/2)
 # print('Half of that is:')
@@ -39,10 +39,100 @@ print('')
 
 # int() ?? What is that?
 # A function!  A built-in piece of code that comes with Python.
-# It's short for INTERGER.  You're telling the computer:
-# "Hey, everything in these parentheses is a number."
+# It's short for INTEGER.  You're telling the computer:
+# "Hey, everything in these parentheses is a whole number."
 # Before we go on, let's check out some other built-in functions at the
 # official Python page:
 # https://docs.python.org/3/library/functions.html
 
 # What other built-in functions have we already used?
+
+
+# The computer recognizes different types of data. 
+# We've already seen an INTEGER
+# A computuer can also understand a decimal number.  This data type is called a FLOAT
+# Uncomment the next section and try this:
+
+# print('Please input an odd integer:')
+# odd_integer = input()
+# print('You input ' + str(odd_integer))
+# half_odd_integer = int(odd_integer)/2
+# print('Half of that is ' + str(half_odd_integer) + ' Its data type is:')
+# print(type(half_odd_integer))
+
+# A computer can also recognize a LIST
+# Uncomment and run the next TWO sections
+
+# list_of_albums = ["Dangerously in Love",
+#  "B'Day",
+#  "I Am... Sasha Fierce",
+#  "4",
+#  "Beyoncé",
+#  "Lemonade",
+#  "Renaissance",
+#  "Cowboy Carter"
+#  ]
+
+# for album in list_of_albums:
+#     print('I love ' + album)
+
+# Congratulations, you have read a list and run a LOOP
+# Now please write and run your own list and loop below:
+
+
+#################
+
+# OK uncomment list_of_albums so we can refer to it again.
+# And we'll do an IF/ELSE loop:
+
+# favorite = "Lemonade"
+
+# for album in list_of_albums:
+#     if album == favorite:
+#         print(album + ' IS MY FAVORITE!')
+#     else:
+#         print('I love ' + album)
+
+###############
+
+# Let's make our own list and save it
+
+# This is an IMPORT statement. It imports code so common that it's bundled with Python
+
+# import csv
+
+# songs = [
+#     'Formation',
+#     'Telephone'
+# ]
+
+# Let's add a song.  There's an append METHOD.  Any LIST can have items added to it 
+# by the append METHOD
+
+# songs.append('Break My Soul')
+
+# for song in songs:
+#     print(song)
+#     # There's an upper METHOD to strings
+#     # any string can be made uppercase
+#     print(song.upper())
+
+# Now you add a song below
+
+
+#########
+
+# Finally, let's save our list of songs.
+# we're going to use the built-in function OPEN
+# we're going to use the csv MODULE we imported, and its writer function
+# we're going to use a loop 
+# Uncomment the next section and run it
+
+
+# outfile = open('python_files/songs.csv', 'w')
+# writer = csv.writer(outfile)
+# for song in songs:
+#     writer.writerow([song])
+
+
+
