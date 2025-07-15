47b415a6561135b4ccadb418d2e34353fda01996
Maggie Lee <maggie.a.lee@gmail.com>
2025-07-15 02:24:32 +0000

    commented out

--- first_python_scraper.py
+++ first_python_scraper.py
@@ -8,19 +8,19 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:136.0
 response = requests.get(url, headers=headers)
 print(response.status_code)
 
-html = response.content
-# print(html)
+# html = response.content
+# # print(html)
 
-soup = BeautifulSoup(html, features="html.parser")
-# print(soup.prettify())
+# soup = BeautifulSoup(html, features="html.parser")
+# # print(soup.prettify())
 
-# Grab all the <tr> elements where valign = bottom
+# # Grab all the <tr> elements where valign = bottom
 
-rows = soup.find_all('tr', attrs={'valign':'bottom'})
+# rows = soup.find_all('tr', attrs={'valign':'bottom'})
 
-# and print them.  How do they look? 
-for row in rows[0:10]:
-    print(row.get_text())
+# # and print them.  How do they look? 
+# for row in rows[0:10]:
+#     print(row.get_text())
 
 # They look kind of rough.  
 # Let's remember the principle of each cell having just one piece of info.
@@ -69,31 +69,31 @@ for row in rows[0:10]:
 #########################################
 
 
-output = []
+# output = []
 
-for row in rows[0:10]:
-    # start with an empty list. 
-    list_of_cells = []
-    # Then find all the <td> elements on a given row
-    cells = row.find_all('td')
-    for cell in cells:
-        try:
-            link = cell.find('a')
-            print(link)
-            print(link['href'])
-            list_of_cells.append('https://apps.dca.ga.gov/DRI/' + link['href'])
-        except:
-            text = cell.text
-            list_of_cells.append(text)
-    print(list_of_cells)
-    # Now this list of cells, append it to output
-    output.append(list_of_cells)
+# for row in rows[0:10]:
+#     # start with an empty list. 
+#     list_of_cells = []
+#     # Then find all the <td> elements on a given row
+#     cells = row.find_all('td')
+#     for cell in cells:
+#         try:
+#             link = cell.find('a')
+#             print(link)
+#             print(link['href'])
+#             list_of_cells.append('https://apps.dca.ga.gov/DRI/' + link['href'])
+#         except:
+#             text = cell.text
+#             list_of_cells.append(text)
+#     print(list_of_cells)
+#     # Now this list of cells, append it to output
+#     output.append(list_of_cells)
 
 
-outfile = open('python_files/ten_rows_with_links.csv', 'w')
-writer = csv.writer(outfile)
-for row in output:
-    writer.writerow(row)
+# outfile = open('python_files/ten_rows_with_links.csv', 'w')
+# writer = csv.writer(outfile)
+# for row in output:
+#     writer.writerow(row)
 
 ##################
 # GREAT, now you have scraped text and links from <td> elements 
